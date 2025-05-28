bc5be9c03d664d12fda1517622ad4cce7b8df7f0
Tanishk Goyal <goyaltanishk02@gmail.com>
2025-05-28 21:54:26 +0400

    Cleanup Deploy All (#179)
    * chore: remove deployAll
    
    * chore: set pause authority in deploy config
    
    * fix: jq filter

--- script/DeployAll.s.sol
+++ script/DeployAll.s.sol
@@ -2,12 +2,27 @@
 pragma solidity ^0.8.4;
 
 import {Script} from "forge-std/Script.sol";
-import "../src/DeployAll.sol";
+import {Orchestrator} from "../src/Orchestrator.sol";
+import {PortoAccount} from "../src/PortoAccount.sol";
+import {LibEIP7702} from "solady/accounts/LibEIP7702.sol";
+import {AccountRegistry} from "../src/AccountRegistry.sol";
+import {Simulator} from "../src/Simulator.sol";
 
 contract DeployAllScript is Script {
+    address public orchestrator;
+    address public accountImplementation;
+    address public accountProxy;
+    address public accountRegistry;
+    address public simulator;
+
     function run() external {
         vm.startBroadcast();
-        new DeployAll();
+        address pauseAuthority = vm.envAddress("PAUSE_AUTHORITY");
+        orchestrator = address(new Orchestrator(pauseAuthority));
+        accountImplementation = address(new PortoAccount(address(orchestrator)));
+        accountProxy = LibEIP7702.deployProxy(accountImplementation, address(0));
+        accountRegistry = address(new AccountRegistry());
+        simulator = address(new Simulator());
         vm.stopBroadcast();
     }
 }
